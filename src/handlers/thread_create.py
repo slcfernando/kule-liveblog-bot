7f8123596fa15461bb27e5ff62ec0dcaa8632708
Sidney Lance Fernando <scfernando@up.edu.ph>
2026-05-08 18:39:51 +0800

    refactor(`thread_create.py`): turn live blog forum name into environment variable

--- src/handlers/thread_create.py
+++ src/handlers/thread_create.py
@@ -1,11 +1,21 @@
-from discord import Client, Thread
+import os
+from dotenv import load_dotenv
+from discord import Client, Thread, ForumChannel
 
 from services import sheets
+
+load_dotenv()
+LIVE_BLOG_FORUM_NAME = os.getenv('LIVE_BLOG_FORUM_NAME')
 
 def setup(client: Client):
     @client.event
     async def on_thread_create(thread: Thread):
-        if thread.parent.name == 'kule-liveblog-bot-test':
+        # Ignore threads not created in a forum
+        if thread.parent is not ForumChannel:
+            return
+        
+        # Verify that forum is the one for live blogs
+        if thread.parent.name == LIVE_BLOG_FORUM_NAME:
             print(f'Thread {thread.name} was created')
 
             # Create new sheet in the spreadsheet using the thread's name
