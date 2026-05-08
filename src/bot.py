94f88705a266601e78ecaaa69b74bd8c52298f43
Sidney Lance Fernando <scfernando@up.edu.ph>
2026-05-08 15:22:26 +0800

    feat: add starter `bot.py` code

---
+++ src/bot.py
@@ -0,0 +1,27 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+# Setup bot token
+load_dotenv()
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello world!')
+
+client.run(BOT_TOKEN)
