da3f487c01f8a48bb3c99264a6aa18a75667cc85
Sidney Lance Fernando <scfernando@up.edu.ph>
2026-05-08 18:08:05 +0800

    feat(`sheets.py`: add services for connecting to Sheets API and creating sheet

---
+++ src/services/sheets.py
@@ -0,0 +1,45 @@
+import os
+from dotenv import load_dotenv
+from google.oauth2 import service_account
+from googleapiclient.discovery import build, Resource
+
+# Get environment variables needed for Google Sheets API
+load_dotenv()
+
+GOOGLE_SHEETS_API_JSON_FILE_PATH = os.getenv('GOOGLE_SHEETS_API_JSON_FILE_PATH')
+GOOGLE_SPREADSHEET_ID = os.getenv('GOOGLE_SPREADSHEET_ID')
+
+SCOPES = (
+    'https://www.googleapis.com/auth/spreadsheets',
+    )
+
+def connect_to_sheets_api() -> Resource:
+    creds = service_account.Credentials.from_service_account_file(
+        GOOGLE_SHEETS_API_JSON_FILE_PATH,
+        scopes=SCOPES
+    )
+    service = build('sheets', 'v4', credentials=creds)
+    return service
+
+def create_sheet(title: str):
+    service = connect_to_sheets_api()
+
+    body = {
+        'requests': [
+            {
+                'addSheet': {
+                    'properties': {
+                        'title': title,
+                        'index': 0
+                    }
+                }
+            }
+        ]
+    }
+
+    response = service.spreadsheets().batchUpdate(
+        spreadsheetId=GOOGLE_SPREADSHEET_ID,
+        body=body
+    ).execute()
+
+    return response
