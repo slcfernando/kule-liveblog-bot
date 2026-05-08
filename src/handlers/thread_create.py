6b10de6cb0530a28cef2242ba783a8049c7c3524
Sidney Lance Fernando <scfernando@up.edu.ph>
2026-05-08 22:52:45 +0800

    refactor(`thread_create.py`): add error handlign

--- src/handlers/thread_create.py
+++ src/handlers/thread_create.py
@@ -12,10 +12,12 @@ LIVE_BLOG_FORUM_NAME = os.getenv('LIVE_BLOG_FORUM_NAME')
 def setup(client: Client, service: Resource):
     @client.event
     async def on_thread_create(thread: Thread):
-        print(f'New thread was created with name {thread}')
-        
-        # Verify that forum is the one for live blogs
-        if thread.parent.name == LIVE_BLOG_FORUM_NAME:
-            # Create new sheet in the spreadsheet using the thread's name
-            sheets.create_sheet(service, thread.name)
-            sheets.initialize_sheet(service, thread.name, thread.jump_url)
+        try:        
+            # Verify that forum is the one for live blogs
+            if thread.parent.name == LIVE_BLOG_FORUM_NAME:
+                # Create new sheet in the spreadsheet using the thread's name
+                sheets.create_sheet(service, thread.name)
+                sheets.initialize_sheet(service, thread.name, thread.jump_url)
+        except Exception as e:
+            print(f'An error occured: {e}')
+            await thread.send('An error occurred while setting up this live coverage\'s Google Sheet.')
