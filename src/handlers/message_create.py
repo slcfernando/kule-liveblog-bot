c44f53160cb5a0b7dfcfab48cd99cbc39e73f2f9
Sidney Lance Fernando <scfernando@up.edu.ph>
2026-05-09 16:12:42 +0800

    refactor: check if messages sent in thread starting with `[LIVE]`

--- src/handlers/message_create.py
+++ src/handlers/message_create.py
@@ -3,7 +3,6 @@ from discord import Client, Message
 from googleapiclient.discovery import Resource
 
 from services import sheets
-from utils.config import LIVE_BLOG_FORUM_NAME
 
 def setup(client: Client, service: Resource):
     @client.event
@@ -15,8 +14,8 @@ def setup(client: Client, service: Resource):
         print(f'A message was sent: {message}')
         # Ignore messages not sent in a live blog forum post (which is a thread)
         channel = message.channel
-        if not channel or not channel.parent or channel.parent.name != LIVE_BLOG_FORUM_NAME:
-            print(f'Message not sent in {LIVE_BLOG_FORUM_NAME}')
+        if not channel.name.strip().startswith('[LIVE]'):
+            print(f'Message not sent in a [LIVE] forum post')
             return
     
         try:        
--- src/handlers/thread_create.py
+++ src/handlers/thread_create.py
@@ -2,14 +2,13 @@ from discord import Client, Thread
 from googleapiclient.discovery import Resource
 
 from services import sheets
-from utils.config import LIVE_BLOG_FORUM_NAME
 
 def setup(client: Client, service: Resource):
     @client.event
     async def on_thread_create(thread: Thread):
         try:        
-            # Verify that forum is the one for live blogs
-            if thread.parent.name == LIVE_BLOG_FORUM_NAME:
+            # Verify that forum post is one for live coverage
+            if thread.name.strip().startswith('[LIVE]'):
                 # Create new sheet in the spreadsheet using the thread's name
                 sheetId = sheets.create_sheet(service, thread.name)
                 sheets.initialize_sheet(service, thread.name, thread.jump_url, sheetId)
