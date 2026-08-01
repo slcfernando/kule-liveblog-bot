15347075e302db19e8509e275ca27eb3e83c17c3
Sidney Lance Fernando <scfernando@up.edu.ph>
2026-08-01 10:07:10 +0800

    chore: correct test message

--- src/handlers/bot_commands.py
+++ src/handlers/bot_commands.py
@@ -53,6 +53,6 @@ async def help(channel: Channel):
 
 async def test(channel: Channel):
     try:
-        await channel.send("Maayong adlaw SoliCon/GASC delegates! \:vyansablay:")
+        await channel.send("Maayong adlaw UP Mindanao! <:vyansablay:1526094164700168253>")
     except Exception as e:
         print(f"An error occurred while the bot tried to send a message: {e}")
